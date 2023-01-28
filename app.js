fc68d1ae0f78d54f0121d9e7fd920c194083e5cb
Andric Tham <andrictham@gmail.com>
2023-01-28 19:29:30 +0800

    60: Use Morgan

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+////////////////////////////////
+// 1) MIDDLEWARES
+////////////////////////////////
+
+app.use(morgan('dev'));
 
 // Middleware that allows Express to read JSON data from the body of a POST request. Out of the box, Express doesn't provide that to us.
 app.use(express.json());
@@ -18,9 +25,9 @@ const toursFile = `${__dirname}/dev-data/data/tours-simple.json`
 // Use a JSON file as a source for our data
 const tours = JSON.parse(fs.readFileSync(toursFile))
 
-//
-// Route handlers
-//
+////////////////////////////////
+// 2) ROUTE HANDLERS
+////////////////////////////////
 const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -113,9 +120,9 @@ const deleteTour = (req, res) => {
   })
 }
 
-//
-// Routes
-//
+////////////////////////////////
+// 3) ROUTES
+////////////////////////////////
 
 app.route("/api/v1/tours")
   .get(getAllTours)
@@ -132,6 +139,9 @@ app.route("/api/v1/tours/:id")
 // app.patch("/api/v1/tours/:id", updateTour)
 // app.delete("/api/v1/tours/:id", deleteTour)
 
+////////////////////////////////
+// 4) START SERVER
+////////////////////////////////
 
 const port = 3000;
 app.listen(port, () => {
