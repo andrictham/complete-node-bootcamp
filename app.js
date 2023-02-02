dac264361eef5da32a94be5a25eb63efc6e703eb
Andric Tham <andrictham@gmail.com>
2023-02-02 19:20:04 +0800

    66: Serve static files

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ const app = express();
 // MIDDLEWARES
 ////////////////////////////////
 
+// Use Morgan for logging
 app.use(morgan('dev'));
+
+// Serve static files from the public folder
+// All files will be served on the root of the server
+// e.g. `__dirname/public/img/favicon.png` will be served on `example.com/img/favicon.png`.
+app.use(express.static(`${__dirname}/public`));
 
 // Middleware that allows Express to read JSON data from the body of a POST request. Out of the box, Express doesn't provide that to us.
 app.use(express.json());
