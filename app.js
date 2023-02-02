3ba1cbd7878048b9a55ad8673ece9f5a790b43f4
Andric Tham <andrictham@gmail.com>
2023-02-02 15:14:20 +0800

    61: Implement routes for Users (WIP)

--- app.js
+++ app.js
@@ -120,24 +120,66 @@ const deleteTour = (req, res) => {
   })
 }
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
 ////////////////////////////////
 // 3) ROUTES
 ////////////////////////////////
 
-app.route("/api/v1/tours")
+app
+  .route('/api/v1/tours')
   .get(getAllTours)
-  .post(createTour)
+  .post(createTour);
 
-app.route("/api/v1/tours/:id")
+app
+  .route('/api/v1/tours/:id')
   .get(getTour)
   .patch(updateTour)
-  .delete(deleteTour)
+  .delete(deleteTour);
 
-// app.get("/api/v1/tours", getAllTours)
-// app.get("/api/v1/tours/:id", getTour)
-// app.post("/api/v1/tours", createTour)
-// app.patch("/api/v1/tours/:id", updateTour)
-// app.delete("/api/v1/tours/:id", deleteTour)
+app
+  .route('/api/v1/users')
+  .get(getAllUsers)
+  .post(createUser);
+
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 ////////////////////////////////
 // 4) START SERVER
