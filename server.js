95093f2fb90e35005facb1775d70c0c630a3cf91
Andric Tham <andrictham@gmail.com>
2023-02-09 19:55:30 +0800

    83: Connect to MongoDB using Mongoose

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+////////////////////////////////
+// CONNECT TO MONGODB
+////////////////////////////////
+
+const database = process.env.DATABASE_URI.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(database, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    // eslint-disable-next-line no-console
+    console.log(
+      `🗄️  Database connection successful: ${con.connection.db.databaseName}`
+    );
+  });
 
 ////////////////////////////////
 // START SERVER
