6c4baac9071d0e66617d96006434c80be81e0075
Andric Tham <andrictham@gmail.com>
2023-02-10 20:50:17 +0800

    86: Simple Tour Model + Creating Documents

--- server.js
+++ server.js
@@ -28,6 +28,39 @@ mongoose
     );
   });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  rating: 3.7,
+  price: 1420,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(`✅ Tour saved: ${doc}`);
+  })
+  .catch((err) => {
+    console.log(`❌ Error: ${err}`);
+  });
+
 ////////////////////////////////
 // START SERVER
 ////////////////////////////////
