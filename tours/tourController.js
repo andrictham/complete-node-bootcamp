80296c588fa28a14541e87dee642a461fd895466
Andric Tham <andrictham@gmail.com>
2023-02-18 17:03:42 +0800

    92: Implement update and delete tour using Mongoose

--- tours/tourController.js
+++ tours/tourController.js
@@ -63,18 +63,42 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: `<Updated tour here>`,
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(
+      req.params.id, // The id of the document to be updated
+      req.body, // The updated document contents
+      {
+        new: true, // Returns the newly-updated document
+        runValidators: true, // Run validators on the document again on each update
+      }
+    );
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    // 204: No Content
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
